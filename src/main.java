9e86038e7fe008780873fa396999a95d0597262a
LouisGonzalez <louiscrack50@gmail.com>
2020-08-15 22:31:45 -0600

    envio

---
+++ src/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luisGonzalez
+ */
+public class main {
+    
+    public static void main(String[] args){
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+    }
+    
+}
